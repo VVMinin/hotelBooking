0cfe19c413ceab64f9247290cd93ab38e68a03a1
VVMinin <kumayo23@gmail.com>
2025-11-07 16:44:21 +0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/YOUR_REPOSITORY_NAME/' // <--- Добавлена эта строка
+  base: '/hotelProject/' // <--- Обновлено
 })
